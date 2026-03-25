4f39026659af4b0ff3c9fa4e3075b8c8ddddfa7e
Anu-912 <anudariochiredene@gmail.com>
2026-03-25 16:10:05 -0700

    Update Tab.jsx

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -2,22 +2,20 @@
 import React from 'react'
 export const Tab = ({filter,setFilter}) => {
 const tabButtons = ["All", "Active", "Completed"]
-export const Tab = () => {
-
   return (
     <div className='flex gap-2'>
-{tabButtons.map((tab)=> 
+{tabButtons.map((tab) => 
 <button
   key={tab}
-  onClick={() => setFilter(tab)}
-className='h-[32px] rounded-[2px] text-[14px] bg-gray-200 text-gray-700 font-medium transition-colors hover:bg-blue-600 hover:text-white '
-${ filter === tab
+  onClick={() => setFilter(tab.toLowerCase())}
+className={`h-[32px] rounded-[2px] text-[14px] bg-gray-200 text-gray-700 font-medium transition-colors 
+${ filter === tab.toLowerCase()
   ? 'bg-blue-600 text-white'
   : 'bg-gray-100 text-gray-600'}`} 
   >
-{tab.chartAt(0).toUpperCase() + tab.slice(1)}
+{tab}
 </button>
-))}
+)}
     </div>
   )
 }
