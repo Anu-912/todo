58069d48466aaca3f65066fca0b675371e2a5476
Anu-912 <anudariochiredene@gmail.com>
2026-03-25 12:32:53 +0800

    Update Input.jsx

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,13 +1,16 @@
+"use client"
 import React from 'react'
 
-export const Input = () => {
+export const Input = ({setInputValue}) => {
+  
+  const handleChange = (event) => {
+    setInputValue(event.target.value)
+  }
+  
   return (
-    <div className='flex gap-2 w-full'>
-<input type='text' className='w-[200px] h-[40px] rounded-[8px] border border-gray-300 bg-white text-[16px] text-gray-700 outline-none '>
+<input onChange={handleChange} type='text' className='w-[255px] h-[40px] rounded-[8px] border border-gray-300 bg-white text-[16px] text-gray-700 outline-none px-2 '>
 </input>
-<button className='w-[70px] h-[40px] bg-blue-600 text-white text-[16px] font-medium rounded-[8px]'>
-Add
-</button>
-    </div>
+
   )
 }
+
