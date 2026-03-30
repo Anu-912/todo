c576c0bc30366e454fdc1c70cd551688e8c9aa00
Anu-912 <anudariochiredene@gmail.com>
2026-03-30 10:25:14 +0800

    Update Tab.jsx

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -1,21 +1,23 @@
-"use client"
-import React from 'react'
-export const Tab = ({filter,setFilter}) => {
-const tabButtons = ["All", "Active", "Completed"]
+"use client";
+import React from "react";
+export const Tab = ({ filter, setFilter }) => {
+  const tabButtons = ["All", "Active", "Completed"];
   return (
-    <div className='flex gap-2'>
-{tabButtons.map((tab) => 
-<button
-  key={tab}
-  onClick={() => setFilter(tab.toLowerCase())}
-className={`h-[32px] rounded-[2px] text-[14px] bg-gray-200 text-gray-700 font-medium transition-colors 
-${ filter === tab.toLowerCase()
-  ? 'bg-blue-600 text-white'
-  : 'bg-gray-100 text-gray-600'}`} 
-  >
-{tab}
-</button>
-)}
+    <div className="flex gap-2">
+      {tabButtons.map((tab) => (
+        <button
+          key={tab}
+          onClick={() => setFilter(tab.toLowerCase())}
+          className={`h-[32px] rounded-[2px] text-[14px] bg-gray-200 text-gray-700 font-medium transition-colors 
+${
+  filter === tab.toLowerCase()
+    ? "bg-blue-600 text-white"
+    : "bg-gray-100 text-gray-600"
+}`}
+        >
+          {tab}
+        </button>
+      ))}
     </div>
-  )
-}
+  );
+};
