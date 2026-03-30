8d3a1c30afaa5ee688fb1e771d75a69a8304333e
Anu-912 <anudariochiredene@gmail.com>
2026-03-30 16:36:06 -0700

    Update Input.jsx

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,16 +1,17 @@
 "use client"
 import React from 'react'
 
-export const Input = ({setInputValue}) => {
-  
-  const handleChange = (event) => {
-    setInputValue(event.target.value)
-  }
+export const Input = ({setInputValue, InputValue}) => {
   
   return (
-<input onChange={handleChange} type='text' className='w-[255px] h-[40px] rounded-[8px] border border-gray-300 bg-white text-[16px] text-gray-700 outline-none px-2 '>
+<input 
+value={InputValue}
+onChange={(e) => setInputValue(e.target.value)} 
+placeholder='Add a new task...'
+type='text'
+ className='w-[255px] h-[40px] rounded-[8px] border border-gray-300 bg-white text-[16px] text-gray-700 outline-none px-2 '>
 </input>
 
-  )
-}
+  );
+};
 
