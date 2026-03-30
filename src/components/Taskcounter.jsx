17db4d91080bed3e75f41de0b0f369bde6518d9c
Anu-912 <anudariochiredene@gmail.com>
2026-03-30 16:34:06 -0700

    Update Taskcounter.jsx

--- src/components/Taskcounter.jsx
+++ src/components/Taskcounter.jsx
@@ -1,40 +1,15 @@
 "use client";
 import React from "react";
 
-export const Taskcontainer = ({ tasks, handleDelete, handleCheck }) => {
-  if (tasks.length === 0)
-    return (<p className="text-gray-200 text-[20px] "> Add one above! </p>);
+export const Taskcounter = ({ completedCount, allCount, deleteAllDone }) => {
   return (
-    <div className="flex flex-col w-full gap-2">
-      {tasks.map((task) => (
-        <div
-          key={task.id}
-          className="w-full flex bg-slate-200 rounded-md justify-between p-4"
-        >
-          <div className="flex gap-2">
-            <input
-              type="checkbox"
-              checked={task.Iscompleted}
-              className="size-5"
-            onChange={() => handleCheck(task.id)}
-            ></input>
-            <p
-              className="text-xl"
-              style={{
-                textDecoration: task.Iscompleted ? "line-throgh" : "",
-              }}
-            >
-              {task.InputName}
-            </p>
-            <button
-              onClick={() => handleDelete(task.id)}
-              className="text-red-600 bg-red-200 font-semibold"
-            >
-              Delete
-            </button>
-          </div>
-        </div>
-      ))}
+    <div className="flex justify-between border-t-2 border-amber-50 pt-4">
+      <p>
+        {completedCount} of {allCount} tasks completed
+      </p>
+      <p onClick={deleteAllDone} className="text-red-600  font-semibold px-2">
+        Clear completed
+      </p>
     </div>
   );
 };
