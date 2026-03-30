f732213620d6f1590447b8c3258b60de4e325481
Anu-912 <anudariochiredene@gmail.com>
2026-03-30 10:24:06 +0800

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -5,15 +5,31 @@ import { Taskcontainer } from "@/components/Taskcontainer";
 import { useState } from "react";
 
 export default function Home() {
+  const [filter, setFilter] = useState("all");
   const [InputValue, setInputValue] = useState("");
   console.log(InputValue);
-
   const [tasks, settasks] = useState([]);
   const handleClick = () => {
-    settasks([
-      { InputName: InputValue, Iscompleted: false, id: Date.now() },
-      ...tasks,
-    ]);
+    if (InputValue === "" && InputValue.length === 0) {
+      alert("write task name");
+      return;
+    }
+  };
+  settasks([
+    { InputName: InputValue, Iscompleted: false, id: Date.now() },
+    ...tasks,
+  ]);
+  setInputValue("");
+
+  const handleCheck = (taskId) => {
+    const updatedTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, Iscompleted: !task.Iscompleted };
+      } else {
+        return task;
+      }
+    });
+    settasks(updatedTasks);
   };
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-200">
@@ -22,7 +38,7 @@ export default function Home() {
           To-Do list
         </p>
         <div className="flex gap-3 w-full">
-          <Input setInputValue={setInputValue} />
+          <Input setInputValue={setInputValue} InputValue={InputValue} />
           <button
             onClick={handleClick}
             className="w-[70px] h-[40px] bg-blue-600 text-white text-[16px] font-medium rounded-[8px]"
@@ -31,22 +47,22 @@ export default function Home() {
           </button>
         </div>
 
-        <Tab />
+        <Tab filter={filter} setFilter={setFilter} />
         {tasks.map((task, index) => {
           return (
             <div key={index}>
               <input
                 type="checkbox"
                 checked={task.Iscompleted}
-                onChange={  }
+                onChange={handleClick}
                 className="h-[20px] w-[20px] bg-white rounded-[5px]"
+                handleCheck={() => handleCheck(task.id)}
               />
-
               <p>{task.InputName}</p>
             </div>
           );
         })}
-        {/* <Taskcontainer/> */}
+        <Taskcontainer />
         <p className="text-center text-gray-500 text[13px]">
           Powered by{" "}
           <a href="#" className="text-blue-500">
@@ -57,4 +73,3 @@ export default function Home() {
     </div>
   );
 }
-
