24872b76901b69b130e93fe19a71b1ae88d4e96d
Anu-912 <anudariochiredene@gmail.com>
2026-03-30 12:16:37 +0800

    Update Tasks.jsx

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+"use client";
+import React from "react";
 
-export const Tasks = () => {
+export const Taskcontainer = ({ tasks, handleDelete, handleCheck }) => {
+  if (tasks.length === 0)
+    return <p className="text-gray-200 text-[20px] "> Add one above! </p>;
   return (
-    <div>Tasks</div>
-  )
-}
+    <div className="flex flex-col w-full gap-2">
+      {tasks.map((task) => (
+        <div
+          key={task.id}
+          className="w-full flex bg-slate-200 rounded-md justify-between p-4"
+        >
+          <div className="flex gap-2">
+            <input
+              type="checkbox"
+              checked={task.Iscompleted}
+              className="size-8"
+              onClick={() => handleCheck(task.id)}
+            ></input>
+            <p
+              className="text-xl"
+              style={{
+                textDecoration: Iscompleted ? "line-throgh" : "",
+              }}
+            >
+              {taskName}
+            </p>
+            <button
+              onClick={() => handleDelete(task.id)}
+              className="text-red-600 bg-red-200 font-semibold"
+            >
+              Delete
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
