b3a8a068e4dd5240fb0f5f589e46da488dd6599f
Anu-912 <anudariochiredene@gmail.com>
2026-03-30 12:14:08 +0800

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,12 @@
 import { Input } from "@/components/Input";
 import { Tab } from "@/components/Tab";
 import { Taskcontainer } from "@/components/Taskcontainer";
+import { Taskcounter } from "@/components/Taskcounter";
 import { useState } from "react";
 
 export default function Home() {
   const [filter, setFilter] = useState("all");
   const [InputValue, setInputValue] = useState("");
-  console.log(InputValue);
   const [tasks, settasks] = useState([]);
   const handleClick = () => {
     if (InputValue === "" && InputValue.length === 0) {
@@ -31,6 +31,17 @@ export default function Home() {
     });
     settasks(updatedTasks);
   };
+  const handleDelete = (taskId) => {
+    settasks(tasks.filter((task) => task.id !== taskId));
+  };
+
+  const filteredTasks = tasks.filter((task) => {
+    if (currentFilter === "Active") return !task.isCompleted;
+    if (currentFilter === "Completed") return task.isCompleted;
+
+    return true;
+  });
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-200">
       <div className="w-[377px] h-[290px] bg-white rounded-[12px] p-6 flex flex-col gap-4">
@@ -62,7 +73,13 @@ export default function Home() {
             </div>
           );
         })}
-        <Taskcontainer />
+        <Taskcounter completedCount={completedCount} allCount={allCount} />
+        <Taskcontainer
+          tasks={filteredTasks}
+          handleCheck={handleCheck}
+          handleDelte={handleDelete}
+          deleteAllDone={deleteAllDone}
+        />
         <p className="text-center text-gray-500 text[13px]">
           Powered by{" "}
           <a href="#" className="text-blue-500">
